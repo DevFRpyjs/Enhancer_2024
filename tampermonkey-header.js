f4a86b25d8cda3d85b0e09332dd3a043cf4d2743
DevFRpyjs <winxpfam@hotmail.fr>
2023-09-25 22:53:57 +0200

    20240925

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -2,9 +2,9 @@ module.exports = {
   headers: {
     name: "FUT Trade Enhancer",
     namespace: "http://tampermonkey.net/",
-    version: "1.1.5",
+    version: "20240925",
     description: "FUT Trade Enhancer",
-    author: "CK Algos",
+    author: "DevFRpyjs",
     match: ["https://www.ea.com/ea-sports-fc/ultimate-team/web-app/*"],
     grant: ["GM_xmlhttpRequest", "GM_download"],
     connect: ["ea.com", "ea2.com", "futbin.com", "amazonaws.com"],
